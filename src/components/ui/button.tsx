f30ea5555fb72b3dfb7ef88bc60d7eed9d2ef1b5
Abhijeet <abhijeetkumar7011@gmail.com>
2026-06-08 04:43:32 +0530

    fixes

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export function Button({
+  className,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={className}
+      {...props}
+    />
+  );
+}
